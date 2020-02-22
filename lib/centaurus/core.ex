75c704c499e7d3f76295e5f84f041cd7cada059b
mischief901 <alex.misch@tulip.co>
2020-02-22 14:03:02 -0500

    Actually finish adjusting the nif functions.

--- lib/centaurus/core.ex
+++ lib/centaurus/core.ex
@@ -137,21 +137,21 @@ defmodule Centaurus.Core do
 
     @nif_error :erlang.nif_error(:nif_not_loaded)
     
-    def accept_nif(_lsocket, _opts), do: @nif_error
+    def accept_nif(_quic_socket, _timeout), do: @nif_error
 
-    def connect_nif(_ip, _port, _timeout, _opts), do: @nif_error
+    def connect_nif(_quic_socket, _timeout), do: @nif_error
 
-    def close_nif(_socket), do: @nif_error
+    def close_nif(_quic_socket), do: @nif_error
 
-    def close_stream_nif(_stream), do: @nif_error
+    def close_stream_nif(_quic_stream), do: @nif_error
 
-    def listen_nif(_port, _opts), do: @nif_error
+    def listen_nif(_quic_socket), do: @nif_error
 
-    def open_stream_nif(_socket, _opts), do: @nif_error
+    def open_stream_nif(_quic_socket, _direction), do: @nif_error
 
-    def read_nif(_stream, _timeout), do: @nif_error
+    def read_nif(_quic_stream, _timeout), do: @nif_error
 
-    def write_nif(_stream, _data), do: @nif_error
+    def write_nif(_quic_stream, _data), do: @nif_error
     
   end
   
