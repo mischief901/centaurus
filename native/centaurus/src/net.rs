fbfab72440cbe3a37d0b22d0f4003ff4b00f0bf1
mischief901 <alex.misch901@gmail.com>
2020-03-03 23:46:17 -0500

    Add some description of Net fields.

--- native/centaurus/src/net.rs
+++ native/centaurus/src/net.rs
@@ -13,21 +13,30 @@ use std::{
 use quinn::{ EndpointBuilder, NewConnection, ServerConfig };
 
 pub trait Net {
+    // Error Type
     type E;
+    // Stream Type
     type S;
-    
+
+    // Returns the socket address to connect with.
     fn address(&self) -> Option<SocketAddr>;
 
+    // Configures a client endpoint
     fn configure_client(&self) -> Result<EndpointBuilder, Self::E>;
 
+    // Configures a server endpoint
     fn configure_server(&self) -> Result<ServerConfig, Self::E>;
 
+    // Called by the server when a new connection is accepted
     fn new_connection(&self, connection : NewConnection, ctx : &mut Context) -> ();
-    
+
+    // The server name of the connection
     fn server_name(&self) -> &str;
 
-    fn new_peer_stream(&self) -> ();
+    // Called when your peer opens a new stream
+    fn new_peer_stream(&self) -> Result<Self::S, Self::E>;
 
+    // Called when you open a new stream
     fn new_owned_stream(&self) -> Result<Self::S, Self::E>;
 }
 
